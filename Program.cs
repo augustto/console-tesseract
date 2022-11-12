f644b099529b38101c01c176619a52f731149cde
augustto <auguztocezar@gmail.com>
2022-11-11 21:45:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleTesseractOcr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // path ocr, lang, path ocr langdata
+            var service = new TesseractService(@"D:\programs\TesseractOcr", "eng", @"D:\programs\TesseractOcr\tessdata");
+
+            var stream = File.OpenRead("D:\\DEV\\File\\TesseractOCRWebApi\\ocr\\tests\\8.jpg");
+            var text = service.GetText(stream);
+            
+            Console.WriteLine(text);
+            Console.Read();
+
+        }
+    }
+}
